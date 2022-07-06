dbf897fb9ce81f3eb9ed6f1bf0eb506caeec4958
luqin2007 <lq2007lq@hotmail.com>
2022-07-06 10:00:04 +0800

    fix: recipe type class mistake

--- src/api/java/games/moegirl/sinocraft/sinocore/api/crafting/AbstractRecipeSerializer.java
+++ src/api/java/games/moegirl/sinocraft/sinocore/api/crafting/AbstractRecipeSerializer.java
@@ -1,18 +1,15 @@
 package games.moegirl.sinocraft.sinocore.api.crafting;
 
-import com.google.common.reflect.TypeToken;
 import com.google.gson.JsonObject;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
+import java.lang.reflect.ParameterizedType;
+
 public abstract class AbstractRecipeSerializer<T extends Recipe<?>> extends ForgeRegistryEntry<RecipeSerializer<?>> implements RecipeSerializer<T> {
 
-    private final Class<T> type;
-
-    public AbstractRecipeSerializer() {
-        type = (Class<T>) TypeToken.of(getClass()).getRawType();
-    }
+    protected Class<T> type = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
 
     public abstract void toJson(JsonObject pJson, T pRecipe);
 
