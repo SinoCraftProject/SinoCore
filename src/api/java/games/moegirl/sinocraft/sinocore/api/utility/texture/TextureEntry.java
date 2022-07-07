52abde622f396bae1a25d32036b0a452c8bb9e47
luqin2007 <lq2007lq@hotmail.com>
2022-07-07 23:51:57 +0800

    feat: add normalized to texture

--- src/api/java/games/moegirl/sinocraft/sinocore/api/utility/texture/TextureEntry.java
+++ src/api/java/games/moegirl/sinocraft/sinocore/api/utility/texture/TextureEntry.java
@@ -1,4 +1,8 @@
 package games.moegirl.sinocraft.sinocore.api.utility.texture;
 
 public record TextureEntry(String name, int x, int y, int w, int h, float u, float v, int tw, int th) {
+
+    public Normalized normalized(TextureMap texture) {
+        return new Normalized(texture, u, v, tw, th);
+    }
 }
