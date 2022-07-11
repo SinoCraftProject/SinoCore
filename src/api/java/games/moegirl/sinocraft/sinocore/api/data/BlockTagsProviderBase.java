14953376f5533572f126bf5a563e732056505e73
luqin2007 <lq2007lq@hotmail.com>
2022-07-11 08:41:59 +0800

    fix: tag namespace

--- src/api/java/games/moegirl/sinocraft/sinocore/api/data/BlockTagsProviderBase.java
+++ src/api/java/games/moegirl/sinocraft/sinocore/api/data/BlockTagsProviderBase.java
@@ -1,43 +1,61 @@
 package games.moegirl.sinocraft.sinocore.api.data;
 
-import games.moegirl.sinocraft.sinocore.api.data.base.ExtendedBlockTagsProvider;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagManager;
+import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import org.jetbrains.annotations.Nullable;
 
-import java.nio.file.Path;
+import java.util.Collection;
+import java.util.stream.Stream;
 
-/**
- * Register block tags
- *
- * @author skyinr
- */
-public abstract class BlockTagsProviderBase extends ExtendedBlockTagsProvider {
-    protected String mainModId;
+public abstract class BlockTagsProviderBase extends BlockTagsProvider {
 
-    public BlockTagsProviderBase(DataGenerator pGenerator, String modId, @Nullable ExistingFileHelper existingFileHelper) {
-        super(pGenerator, modId, existingFileHelper);
-    }
-
-    public BlockTagsProviderBase(DataGenerator pGenerator, String modId, @Nullable ExistingFileHelper existingFileHelper, String mainModIdIn) {
-        super(pGenerator, modId, existingFileHelper);
-        mainModId = mainModIdIn;
+    public BlockTagsProviderBase(DataGenerator generator, String modId, ExistingFileHelper existingFileHelper) {
+        super(generator, modId, existingFileHelper);
     }
 
     @Override
-    public String getName() {
-        return "Mod " + modId + " Block Tags";
+    protected abstract void addTags();
+
+    public void addPickaxe(Block... blocks) {
+        add(BlockTags.MINEABLE_WITH_PICKAXE, blocks);
     }
 
-    @Override
-    protected Path getPath(ResourceLocation loc) {
-        var key = this.registry.key();
-        return this.generator.getOutputFolder().resolve("data/" + getMainModId() + "/" + TagManager.getTagDir(key) + "/" + loc.getPath() + ".json");
+    public void addAxe(Block... blocks) {
+        add(BlockTags.MINEABLE_WITH_AXE, blocks);
     }
 
-    public String getMainModId() {
-        return mainModId != null ? mainModId : modId;
+    public void addShovel(Block... blocks) {
+        add(BlockTags.MINEABLE_WITH_SHOVEL, blocks);
+    }
+
+    public void addHoe(Block... blocks) {
+        add(BlockTags.MINEABLE_WITH_HOE, blocks);
+    }
+
+    public void addStoneTool(Block... blocks) {
+        add(BlockTags.NEEDS_STONE_TOOL, blocks);
+    }
+
+    public void addIronTool(Block... blocks) {
+        add(BlockTags.NEEDS_IRON_TOOL, blocks);
+    }
+
+    public void addDiamondTool(Block... blocks) {
+        add(BlockTags.NEEDS_DIAMOND_TOOL, blocks);
+    }
+
+    public void add(TagKey<Block> tag, Block... blocks) {
+        tag(tag).add(blocks);
+    }
+
+    public void add(TagKey<Block> tag, Collection<? extends Block> blocks) {
+        tag(tag).add(blocks.toArray(Block[]::new));
+    }
+
+    public void add(TagKey<Block> tag, Stream<? extends Block> blocks) {
+        tag(tag).add(blocks.toArray(Block[]::new));
     }
 }
--- src/api/java/games/moegirl/sinocraft/sinocore/api/data/ItemTagsProviderBase.java
+++ src/api/java/games/moegirl/sinocraft/sinocore/api/data/ItemTagsProviderBase.java
@@ -2,12 +2,8 @@ package games.moegirl.sinocraft.sinocore.api.data;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagManager;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
-
-import java.nio.file.Path;
 
 /**
  * Register item tags.
@@ -15,22 +11,11 @@ import java.nio.file.Path;
  * @author qyl27
  */
 public abstract class ItemTagsProviderBase extends ItemTagsProvider {
-    protected String mainModId;
 
     public ItemTagsProviderBase(DataGenerator pGenerator, BlockTagsProviderBase blockTags, String modId, @Nullable ExistingFileHelper existingFileHelper) {
         super(pGenerator, blockTags, modId, existingFileHelper);
-
-        mainModId = blockTags.getMainModId();
     }
 
     @Override
-    protected Path getPath(ResourceLocation loc) {
-        var key = this.registry.key();
-        return this.generator.getOutputFolder().resolve("data/" + mainModId + "/" + TagManager.getTagDir(key) + "/" + loc.getPath() + ".json");
-    }
-
-    @Override
-    public String getName() {
-        return "Mod " + modId + " Item Tags";
-    }
+    protected abstract void addTags();
 }
