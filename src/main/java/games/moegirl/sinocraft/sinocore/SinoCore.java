ee0039315b5d8fd7065d6c0b7c01a0251028aee8
秋雨落 <i@rain.cx>
2022-07-27 22:01:01 +0800

    feat: update to 1.2.9.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/games/moegirl/sinocraft/sinocore/SinoCore.java
+++ src/main/java/games/moegirl/sinocraft/sinocore/SinoCore.java
@@ -31,7 +31,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class SinoCore {
     private static final Logger logger = LogManager.getLogger("SinoCore");
     public static final String MODID = "sinocore";
-    public static final String VERSION = "1.2.8";
+    public static final String VERSION = "1.2.9";
 
     private static SinoCore INSTANCE = null;
 
