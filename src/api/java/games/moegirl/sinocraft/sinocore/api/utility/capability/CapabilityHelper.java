b62e620c33d569a5df34eee317d727a4d26f3456
秋雨落 <i@rain.cx>
2022-07-28 22:16:26 +0800

    fix: bugs.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/api/java/games/moegirl/sinocraft/sinocore/api/utility/capability/CapabilityHelper.java
+++ src/api/java/games/moegirl/sinocraft/sinocore/api/utility/capability/CapabilityHelper.java
@@ -1,27 +1,52 @@
 package games.moegirl.sinocraft.sinocore.api.utility.capability;
 
+import games.moegirl.sinocraft.sinocore.api.SinoCoreAPI;
 import games.moegirl.sinocraft.sinocore.api.capability.IPlayerCapability;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
 
+
 public class CapabilityHelper {
     public static void clone(Player newPlayer, Player original,
                              LazyOptional<IPlayerCapability> optional,
                              ICapabilityProvider provider,
                              Capability<IPlayerCapability> capability) {
-        var cap = original
-                .getCapability(capability)
-                .orElse(provider
-                        .getCapability(capability)
-                        .orElseThrow(() -> new RuntimeException("Cannot get capability " + capability.getName())));
-        var newCap = newPlayer
-                .getCapability(capability)
-                .orElse(provider
-                        .getCapability(capability)
-                        .orElseThrow(() -> new RuntimeException("Cannot get capability " + capability.getName())));
+        var capOptional = original
+                .getCapability(capability);
+        if (!capOptional.isPresent()) {
+            capOptional = provider.getCapability(capability);
 
+            if (!capOptional.isPresent()) {
+                SinoCoreAPI.LOGGER.warn("Cannot get capability for " + capability.getName());
+                return;
+            }
+        }
+
+        var cap = capOptional.orElseThrow(null);
+
+        if (cap == null) {
+            return;
+        }
+
+        var newCapOptional = newPlayer
+                .getCapability(capability);
+        if (!newCapOptional.isPresent()) {
+            newCapOptional = provider.getCapability(capability);
+
+            if (!capOptional.isPresent()) {
+                SinoCoreAPI.LOGGER.warn("Cannot get capability for " + capability.getName());
+                return;
+            }
+        }
+
+        var newCap = capOptional.orElseThrow(null);
+
+        if (newCap == null) {
+            return;
+        }
+        
         newCap.deserializeNBT(cap.serializeNBT());
     }
 }
