f7e5c4f52d93b79fc3008eae25b975d34a0e34c2
秋雨落 <i@rain.cx>
2022-08-04 16:43:07 +0800

    version: 1.2.14
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/games/moegirl/sinocraft/sinocore/SinoCore.java
+++ src/main/java/games/moegirl/sinocraft/sinocore/SinoCore.java
@@ -31,7 +31,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class SinoCore {
     private static final Logger logger = LogManager.getLogger("SinoCore");
     public static final String MODID = "sinocore";
-    public static final String VERSION = "1.2.13";
+    public static final String VERSION = "1.2.14";
 
     private static SinoCore INSTANCE = null;
 
