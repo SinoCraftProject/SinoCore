4dae5b1f7ceef2d23c998736d45583c41031d063
秋雨落 <i@rain.cx>
2022-08-09 20:01:40 +0800

    feat: show commit name and dev or release.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/games/moegirl/sinocraft/sinocore/utility/SCConstants.java
+++ src/main/java/games/moegirl/sinocraft/sinocore/utility/SCConstants.java
@@ -1,5 +1,5 @@
 package games.moegirl.sinocraft.sinocore.utility;
 
 public class SCConstants {
-    public static final boolean IS_DEV = Boolean.parseBoolean(System.getProperty("sinocore.dev", "false"));
+    public static final boolean IS_DEV = Boolean.parseBoolean(System.getProperty("sinoseries.dev", "false"));
 }
