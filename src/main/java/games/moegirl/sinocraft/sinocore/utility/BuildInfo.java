21f09e965c2b6fa0f963da9f718f22bf64846b7e
秋雨落 <i@rain.cx>
2025-12-03 18:35:15 +0800

    Change version schema: we have no +mc suffix anymore.
    New version schema: `Major.Minor.Patch`
    Major(changed): Changes for each code-incompatible Minecraft version. (1 for mc 1.21.1)
    Minor: For each breaking-change or functional update of SinoCore. (1 for next release)
    Patch: For bug-fixes.
    
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/games/moegirl/sinocraft/sinocore/utility/BuildInfo.java
+++ src/main/java/games/moegirl/sinocraft/sinocore/utility/BuildInfo.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import java.time.ZonedDateTime;
 
 public class BuildInfo {
-    public static final String FULL_VERSION;
     public static final String MOD_VERSION;
     public static final @Nullable ZonedDateTime BUILD_TIME;
     public static final String COMMIT_ID;
@@ -17,7 +16,6 @@ public class BuildInfo {
 
     static {
         String mod_version;
-        String full_version;
         ZonedDateTime buildTime;
         String commitId;
         boolean release;
@@ -26,19 +24,16 @@ public class BuildInfo {
             var properties = new java.util.Properties();
             properties.load(BuildInfo.class.getResourceAsStream("/build_info.properties"));
             mod_version = properties.getProperty("mod_version");
-            full_version = properties.getProperty("full_version");
             buildTime = ZonedDateTime.parse(properties.getProperty("build_time"));
             commitId = properties.getProperty("commit_id");
             release = "true".equalsIgnoreCase(properties.getProperty("release"));
         } catch (Exception ignored) {
             mod_version = "Unknown";
-            full_version = "Unknown";
             buildTime = null;
             commitId = "uncommited";
             release = false;
         }
 
-        FULL_VERSION = full_version;
         MOD_VERSION = mod_version;
         BUILD_TIME = buildTime;
         COMMIT_ID = commitId;
@@ -46,6 +41,6 @@ public class BuildInfo {
     }
 
     public static void printBuildInfo() {
-        LOGGER.info("SinoCore Version: {} (Commit {}), Build at: {}", FULL_VERSION, COMMIT_ID, BUILD_TIME != null ? BUILD_TIME : "B.C. 3200");
+        LOGGER.info("SinoCore Version: {} (Commit {}), Build at: {}", MOD_VERSION, COMMIT_ID, BUILD_TIME != null ? BUILD_TIME : "B.C. 3200");
     }
 }
